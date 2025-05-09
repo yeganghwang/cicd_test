828c8cbef917830db881d7d0d55e1287d5cd720d
yeganghwang <ghkdtlahs@icloud.com>
2025-05-09 10:25:48 +0900

    test commit

--- test/app.test.js
+++ test/app.test.js
@@ -4,6 +4,6 @@ const app = require('../app');
 describe('GET /', () => {
   it('responds with Hello CI/CD!', async () => {
     const res = await request(app).get('/');
-    expect(res.text).toBe('Hello CI/CD!');
+    expect(res.text).toBe('Hello CI/CD!!!!!!!');
   });
 });
